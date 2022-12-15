e32fa40f1241da64c943943d23f9a8f086710dd0
Brice Bell <bricebell18@gmail.com>
2022-12-16 02:33:22 +1100

    Publish

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
 
 
-function SetPage(page,ele){
+function SetPage(page, ele) {
 
-   var collection =  document.getElementsByClassName("BarBottem")[0].children;
-    for (let i = 0; i < collection.length; i++) {
-  var tEle = collection[i]
-  tEle.className =""
-}
-ele.className ="selected"
+  var collection = document.getElementsByClassName("navBar")[0].children;
+  for (let i = 0; i < collection.length; i++) {
+    var tEle = collection[i].children[0]
+    var TEMPVar = tEle.className
+    console.log(tEle)
+    // if (TEMPVar.includes("active")) {
+    tEle.className = tEle.className.replaceAll("active", '')
+    //   console.log(tEle.className)
+    // } else {
+
+    // }
+  }
+  ele.className = "active"
 
 
 }
