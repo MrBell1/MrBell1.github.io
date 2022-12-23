d2b030ceaa833051ab8e511754127e3858a79594
Brice Bell <bricebell18@gmail.com>
2022-12-23 16:31:27 +1100

    [Fri 23/12/2022 --16:31:27.01] Publish

--- js/tuya.js
+++ js/tuya.js
@@ -1,6 +1,6 @@
 var devices = {}
 var TuyaReady = false;
-
+var MyID = ((window.location + "")+":"+Math.floor(Math.random() * Math.floor(Math.random() * 100)))
 
 function TuyaStart() {
     // console.log(new Date());
@@ -15,12 +15,14 @@ function TuyaStart() {
                 var TempEle = document.createElement('div')
                 TempEle.id = element.id + ":Basic";
                 TempEle.className = 'TuyaBasic';
-                TempEle.innerHTML = element.name + "<br>" + element.status[0].value
+                document.getElementById("tuyaPage").appendChild(TempEle)
+                TempEle = document.getElementById(element.id + ":Basic")
+                TempEle.innerHTML = element.name + "<br>" + element.status.switch_led
                 TempEle.style.border = "3px solid red"
-                if (element.status[0].value) {
+                if (element.status.switch_led) {
                     TempEle.style.border = "3px solid green"
                 }
-                document.getElementById("tuyaPage").appendChild(TempEle)
+                // document.getElementById("tuyaPage").appendChild(TempEle)
             }
 
 
@@ -73,20 +75,32 @@ function TuyaStart() {
 // TuyaStart();
 function TuyaUpdate() {
     if (Object.keys(devices).length > 0) {
-        
+
         for (let i = 0; i < Object.keys(devices).length; i++) {
             const element = devices['' + i];
             // console.log(element.category)
             if (element.category == "dj") {
-                document.getElementById(element.id + ":Basic").innerHTML = element.name + "<br>" + element.status[0].value
-                if (element.status[0].value) {
+                var TempState = false
+                // document.getElementById(element.id + ":Basic").innerHTML = element.name + "<br>" + element.status.switch_led
+                if (element.status.switch_led) {
                     document.getElementById(element.id + ":Basic").style.border = "3px solid green"
-                }else{
+                    TempState = 'ON'
+                } else {
                     document.getElementById(element.id + ":Basic").style.border = "3px solid red"
+                    TempState = "OFF"
                 }
-                // console.log(element.name)
+                // console.log(element.status.switch_led)
+                document.getElementById(element.id + ":Basic").innerHTML = element.name// + "<div onclick='SwitchLED'>" + TempState+ "</div>"
+                var TempEle = document.createElement("div")
+                TempEle.id = element.id + ":switch"
+                TempEle.innerHTML = TempState
+                document.getElementById(element.id + ":Basic").appendChild(TempEle)
+                TempEle = document.getElementById(element.id + ":switch")
+                TempEle.addEventListener("click",function(){SwitchLED(element.id)})
+             
+
             }
-         
+
 
             // if (element.category == "dj") {
             //     var TempEle = document.createElement('div')
@@ -99,4 +113,8 @@ function TuyaUpdate() {
 
         }
     }
+}
+function SwitchLED(id){
+
+    console.log(webSocket.send(' {"From":"'+MyID+'","type":"tuya","do":"Switch_dj","id":"'+id+'"}'))
 }
